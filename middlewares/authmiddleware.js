aaeb634a4d97336e023e86cac4baf30821aae72b
Paras More <122553673+21PM@users.noreply.github.com>
2024-08-04 10:22:45 +0530

    Update authmiddleware.js

--- middlewares/authmiddleware.js
+++ middlewares/authmiddleware.js
@@ -7,18 +7,19 @@ dotenv.config()
 const authmiddleware = async(req,res,next)=>{
 
     const token = req.cookies.token;
-    
+    console.log("tk",token);
     if(!token){
         return res.status(401).json({
             status:false,
             message:"Please login Invalid token"
         })
     }
-
+  console.log("tk1",token);
 
     try{
         const isVerifiedToken = await jwt.verify(token,process.env.JWTSECRETKEY)  
-        
+          console.log("tk2",isVerifiedToken);
+
         const user  = await userModel.findById(isVerifiedToken.userId)
         if(!user){
             return res.status(401).json({
@@ -26,6 +27,9 @@ const authmiddleware = async(req,res,next)=>{
                 message:"Please login again Invalid token"
             })
         }
+
+          console.log("user",user);
+
 
         req.user = user;
         
